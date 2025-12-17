bd8abd9568073a00732e7ca5ab4b00b4fa65bbf0
j224sugi <j520224z@mails.cc.ehime-u.ac.jp>
2025-12-17 21:26:04 +0900

    ckおよびspoon修正 問題：匿名クラスの後にインナークラスがある場合クラス名がおかしくなる

--- spoon/src/main/java/com/example/Visitor.java
+++ spoon/src/main/java/com/example/Visitor.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 import com.example.calculate.AccessToData;
 import com.example.calculate.BaseClassUsageRation;
@@ -22,6 +21,7 @@ import com.example.node.MethodMetrics;
 
 import spoon.reflect.cu.SourcePosition;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtEnum;
 import spoon.reflect.declaration.CtExecutable;
 import spoon.reflect.declaration.CtInterface;
@@ -71,11 +71,10 @@ public class Visitor extends CtScanner {
         IAttribute superClass = new SuperClass();
         superClass.calculate(classMetrics);
         List<CtExecutable<?>> methods = new ArrayList<>();
-        classMetrics.getDeclaration().getMethods().forEach(a -> methods.add((CtExecutable) a));
-        if (classMetrics.getDeclaration() instanceof CtClass clazz) {
+        ctType.getMethods().forEach(a -> methods.add((CtExecutable) a));
+        if (ctType instanceof CtClass clazz) {
             clazz.getConstructors().forEach(a -> methods.add((CtExecutable) a));
             clazz.getAnonymousExecutables().forEach(a -> methods.add((CtExecutable) a));
-            //methods.add((CtExecutable)clazz.getAnonymousExecutables());
         }
         for (CtExecutable method : methods) {
             MethodMetrics methodMetrics = new MethodMetrics(method, classMetrics);
@@ -91,9 +90,9 @@ public class Visitor extends CtScanner {
 
     }
 
-    public void printCSV(String arg, int flag) throws IOException {
+    public void printCSV(String arg, int isFirst) throws IOException {
         try {
-            if (flag == 0) {
+            if (isFirst == 0) {
                 FileWriter fwClass = new FileWriter(arg + "spoonClass.csv", false);
                 PrintWriter pwClass = new PrintWriter(new BufferedWriter(fwClass));
                 FileWriter fwMethod = new FileWriter(arg + "spoonMethod.csv", false);
@@ -143,98 +142,113 @@ public class Visitor extends CtScanner {
     }
 
     public void printMetrics(PrintWriter pwClass, PrintWriter pwMethod) {
-        String[] metricOfClass = {"NprotM", "BOvR", "ATFD", "ATLD", "LAA", "BUR", "FDP"};
-        String[] metricOfMethod = {"ATFD", "ATLD", "LAA", "FDP"};
         for (CtType clazz : classesMetrics.keySet()) {
-            if(clazz.isAnonymous()){
-                System.out.println(clazz.getQualifiedName());
-            }
-            List<MethodMetrics> initializers = new ArrayList<>();
+            String fileName = getFileName(clazz);
+            String className = getClassName(clazz);
+
             ClassMetrics classMetrics = classesMetrics.get(clazz);
             for (MethodMetrics methodMetrics : classMetrics.getMethodsMetrics()) {
+                boolean isInitializer = false;
                 CtExecutable method = methodMetrics.getDeclaration();
-                String methodName = "";
-                if ("<init>".equals(method.getSimpleName())) {
+                String methodName = method.getSimpleName();
+                if ("<init>".equals(methodName)) {
                     methodName = clazz.getSimpleName();
-                    if (isDigits(methodName)) {
-                        continue;
+                    // if (isDigits(methodName)) {
+                    // continue;
+                    // }
+                } else if ("".equals(methodName)) {
+                    methodName = "(initializer " + getStartLine(method) + ")";
+                    isInitializer = true;
+                }
+
+                printFileClass(pwMethod, fileName, className);
+                pwMethod.print(",");
+                if (!isInitializer) {
+                    List<String> parameters = new ArrayList<>();
+                    List<CtParameter> methodParameter = method.getParameters();
+                    for (CtParameter param : methodParameter) {
+                        parameters.add(param.getType().getQualifiedName());
                     }
-                } else if ("".equals(method.getSimpleName())) {
-                    initializers.add(methodMetrics);
-                    continue;
+                    if (!parameters.isEmpty()) {
+                        pwMethod.print(
+                                "\"" + methodName + "/" + parameters.size() + parameters + "\"");
+                    } else {
+                        pwMethod.print("\"" + methodName + "/" + parameters.size() + "\"");
+                    }
                 } else {
-                    methodName = method.getSimpleName();
+                    pwMethod.print(methodName);
                 }
-                pwMethod.print(clazz.getPosition().getFile().getPath());
+
+                printMethodMetrics(pwMethod, methodMetrics);
                 pwMethod.print(",");
-                pwMethod.print(clazz.getQualifiedName());
-                pwMethod.print(",");
-                List<String> parameters = new ArrayList<>();
-                List<CtParameter> methodParameter = method.getParameters();
-                for (CtParameter param : methodParameter) {
-                    parameters.add(param.getType().getQualifiedName());
-                }
-                if (!parameters.isEmpty()) {
-                    pwMethod.print("\"" + methodName + "/" + parameters.size() + parameters + "\"");
-                } else {
-                    pwMethod.print("\"" + methodName + "/" + parameters.size() + "\"");
-                }
-                for (String metric : metricOfMethod) {
-                    pwMethod.print(",");
-                    pwMethod.print(methodMetrics.getMetric(metric));
-                }
-                pwMethod.print(",");
-                SourcePosition pos = method.getPosition();
-                if (pos != null && pos.isValidPosition()) {
-                    pwMethod.print(pos.getLine());
-                }
+                pwMethod.print(getStartLine(method));
                 pwMethod.println();
             }
-            if (!initializers.isEmpty()) {
-                Map<Integer, MethodMetrics> map = new TreeMap<>();
-                for (MethodMetrics initialize : initializers) {
-                    CtExecutable method = initialize.getDeclaration();
-                    SourcePosition pos = method.getPosition();
-                    if (pos != null && pos.isValidPosition()) {
-                        map.put(method.getPosition().getLine(), initialize);
-                    }
-                }
-                int i = 1;
-                for (Integer line : map.keySet()) {
-                    MethodMetrics methodMetrics = map.get(line);
-                    pwMethod.print(clazz.getPosition().getFile().getPath());
-                    pwMethod.print(",");
-                    pwMethod.print(clazz.getQualifiedName());
-                    pwMethod.print(",");
-                    pwMethod.print("(initializer " + i + ")");
-                    i = i + 1;
-                    for (String metric : metricOfMethod) {
-                        pwMethod.print(",");
-                        pwMethod.print(methodMetrics.getMetric(metric));
-                    }
-                    pwMethod.print(",");
-                    pwMethod.print(line);
-                    pwMethod.println();
-                }
-            }
-            int classLine = 0;
-            if (clazz.getPosition() != null) {
-                SourcePosition position = clazz.getPosition();
-                if (position.getFile() != null && position.isValidPosition()) {
-                    classLine = position.getLine();
-                    pwClass.print(position.getFile().getPath());
-                }
-            }
+
+            printFileClass(pwClass, fileName, className);
+            printClassMetrics(pwClass, classMetrics);
             pwClass.print(",");
-            pwClass.print(clazz.getQualifiedName());
-            for (String metric : metricOfClass) {
-                pwClass.print(",");
-                pwClass.print(classMetrics.getMetric(metric));
-            }
-            pwClass.print(",");
-            pwClass.print(classLine);
+            pwClass.print(getStartLine(clazz));
             pwClass.println();
         }
+    }
+
+    public void printFileClass(PrintWriter pw, String file, String classname) {
+        pw.print(file);
+        pw.print(",");
+        pw.print(classname);
+    }
+
+    public void printMethodMetrics(PrintWriter pwMethod, MethodMetrics methodMetrics) {
+        String[] metricOfMethod = {"ATFD", "ATLD", "LAA", "FDP"};
+        for (String metric : metricOfMethod) {
+            pwMethod.print(",");
+            pwMethod.print(methodMetrics.getMetric(metric));
+        }
+    }
+
+    public void printClassMetrics(PrintWriter pwClass, ClassMetrics classMetrics) {
+        String[] metricOfClass = {"NprotM", "BOvR", "ATFD", "ATLD", "LAA", "BUR", "FDP"};
+        for (String metric : metricOfClass) {
+            pwClass.print(",");
+            pwClass.print(classMetrics.getMetric(metric));
+        }
+    }
+
+    public String getFileName(CtType clazz) {
+        if (clazz.getPosition() != null) {
+            SourcePosition sourcePosition = clazz.getPosition();
+            if (sourcePosition.getFile() != null && sourcePosition.isValidPosition()) {
+                return sourcePosition.getFile().getPath();
+            }
+        }
+        return "null";
+    }
+
+    public String getClassName(CtType clazz) {
+        if (clazz.isAnonymous()) {
+            CtType parent = clazz.getParent(CtType.class);
+            if (parent == null || parent == clazz) {
+                return "$Anonymous$" + getStartLine(clazz);
+            }
+            return getClassName(parent) + "$" + getStartLine(clazz);
+        } else if(clazz.isTopLevel()){
+            return clazz.getQualifiedName();
+        }else{
+            CtType parent=clazz.getParent(CtType.class);
+            if(parent==null||parent==clazz){
+                return "$innerClass$";
+            }
+            return getClassName(parent)+"$"+clazz.getSimpleName();
+        }
+    }
+
+    public int getStartLine(CtElement element) {
+        SourcePosition pos = element.getPosition();
+        if (pos != null && pos.isValidPosition()) {
+            return pos.getLine();
+        }
+        return 0;
     }
 
     public static boolean isDigits(String s) {
@@ -252,14 +266,3 @@ public class Visitor extends CtScanner {
 
 }
 
-/*
- * public void excuteMetrics() { for (CtClass clazz : classesMetrics.keySet()) { //if
- * (Paths.get(clazz.getPosition().getFile().getAbsolutePath()).equals(Paths.get(
- * "C:/Users/syuuj/HikariCP/src/main/java/com/zaxxer/hikari/util/ConcurrentBag.java"))) {
- * ClassMetrics classMetrics = classesMetrics.get(clazz); IAttribute superClass = new SuperClass();
- * superClass.calculate(classMetrics); Set<CtMethod> methods = clazz.getMethods(); for (CtMethod
- * method : methods) { MethodMetrics methodMetrics = new MethodMetrics(method, classMetrics); for
- * (IAttribute metric : metricForMethod) { metric.calculate(methodMetrics); }
- * classMetrics.getMethodsMetrics().add(methodMetrics); } for (IAttribute metric : metricForClass) {
- * metric.calculate(classMetrics); } //} } }
- */
